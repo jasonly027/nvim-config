c3b51865d9e566fc163e545ca3d8c7362efd2803
Jason Ly <81354745+jasonly027@users.noreply.github.com>
2023-08-13 22:06:41 -0700

    Add treesitter indenting and folding

--- lua/jason/plugins/modules/treesitter.lua
+++ lua/jason/plugins/modules/treesitter.lua
@@ -13,6 +13,29 @@ M.opts = {
         enable = true,
         additional_vim_regex_highlighting = false,
     },
+    indent = {
+        enable = true,
+    },
 }
+
+M.config = function()
+    local opt = vim.opt
+
+    -- Line folding using treesitter
+    opt.foldmethod = "expr"
+    opt.foldexpr = "nvim_treesitter#foldexpr()"
+    opt.foldenable = true
+    vim.api.nvim_create_autocmd('BufRead', {
+        callback = function()
+            vim.api.nvim_create_autocmd('BufWinEnter', {
+                once = true,
+                callback = function()
+                    vim.cmd [[normal! zx]]
+                    vim.cmd [[normal! zR]]
+                end
+            })
+        end
+    })
+end
 
 return M
