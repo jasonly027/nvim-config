03133e8bff8ab0607d72c930a516f8f349949f05
Jason Ly <81354745+jasonly027@users.noreply.github.com>
2026-01-02 01:42:01 -0800

    Defer nvim-lspconfig to VimEnter event

--- lua/plugins/code.lua
+++ lua/plugins/code.lua
@@ -11,6 +11,7 @@ return {
   {
     -- Main LSP Configuration
     'neovim/nvim-lspconfig',
+    event = 'VimEnter',
     dependencies = {
       -- Automatically install LSPs and related tools to stdpath for Neovim
       -- Mason must be loaded before its dependents so we need to set it up here.
